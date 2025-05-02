d689d7cd29495136c13a0d351264e29fa1b4bf82
Anton Ivanov <apivanov@avito.ru>
2025-05-02 16:48:21 +0300

    fmt train_rirci.py

--- bin/train_rirci.py
+++ bin/train_rirci.py
@@ -1,14 +1,13 @@
 import torch
 
 from lib.dataset.dataset import get_dataloaders
+from lib.models.rirci import RIRCIModel
+from lib.training.trainer import Trainer
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using: ', device)
 
 train_loader, val_loader = get_dataloaders(data_root='../data/27kpng', batch_size=16)
-
-from lib.training.trainer import Trainer
-from lib.models.rirci import RIRCIModel
 
 trainer = Trainer(RIRCIModel, epochs=100, train_loader=train_loader, val_loader=val_loader, device=device)
 
